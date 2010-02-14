8fc009d1bd6041ff8d8c4062facde4628c90876e
Andrey Petrov <andrey.petrov@shazow.net>
2010-02-14 11:54:31 -0500

    Adding a Client implementation for the websocket module. It works, but it's rough and untested. Need to migrate property assigned callbacks into Emitter-based callbacks.
    Use as such:
    
        var sys = require("sys"),
            websocket = require("./websocket");
    
        var ws = new websocket.Client({'resource': '/'});
        ws.onData = function(data) {
            sys.puts(data);
        };
        ws.onConnect = function() {
            ws.send("hi");
        };

--- websocket.js
+++ websocket.js
@@ -12,35 +12,53 @@ var tcp = require('tcp'),
     sys = require('sys'),
     tools = require('./tools'),
     logger = require('./log'),
-    
+
     // what the request headers should match
-    requestHeaders = [
+    requestHeadersMatch = [
       /^GET (\/[^\s]*) HTTP\/1\.1$/,
       /^Upgrade: WebSocket$/,
       /^Connection: Upgrade$/,
       /^Host: (.+)$/,
       /^Origin: (.+)$/
     ],
-    
+
+    requestHeaders = [
+      'GET {resource} HTTP/1.1',
+      'Upgrade: WebSocket',
+      'Connection: Upgrade',
+      'Host: {host}',
+      'Origin: {origin}',
+      '',
+      ''
+    ],
+
     // what the response headers should be
+    responseHeadersMatch = [
+      /^HTTP\/1\.1 101 Web Socket Protocol Handshake/,
+      /^Upgrade: WebSocket/,
+      /^Connection: Upgrade/,
+      /^WebSocket-Origin: (.+)$/,
+      /^WebSocket-Location: (.+)$/
+    ],
+
     responseHeaders = [
-      'HTTP/1.1 101 Web Socket Protocol Handshake', 
-      'Upgrade: WebSocket', 
+      'HTTP/1.1 101 Web Socket Protocol Handshake',
+      'Upgrade: WebSocket',
       'Connection: Upgrade',
       'WebSocket-Origin: {origin}',
       'WebSocket-Location: {protocol}://{host}{resource}',
       '',
       ''
     ],
-    
-    log = function(message, type){      
+
+    log = function(message, type){
       logger.store('['+ new Date() +'] ['+ (type || 'error') +'] ' + message);
     },
-    
+
     empty = new Function,
-    
+
     Connection,
-    
+
 Server = this.Server = function(options){
   this.options = tools.merge({
     port: 8080,
@@ -50,16 +68,16 @@ Server = this.Server = function(options){
     logKey: null,
     tls: false
   }, options || {});
-  
+
   if (this.options.logKey === null) logger.setKey('node.websocket.' + this.options.host + '.' + this.options.port);
-  
+
   var self = this;
   this.clients = 0;
   this.server = tcp.createServer(function(socket){
-    new Connection(self, socket);    
+    new Connection(self, socket);
   });
   this.server.listen(this.options.port, this.options.host);
-  
+
   if (this.options.log) setInterval(function(){
     sys.puts('['+ new Date() +'] [info] ' + self.clients + ' clients connected', 'info');
   }, 5000);
@@ -78,24 +96,130 @@ Server.prototype._verifyOrigin = function(origin){
 };
 
 Server.prototype._onConnect = function(){
-  this.clients++;  
+  this.clients++;
 };
 
 Server.prototype._onDisconnect = function(){
   this.clients--;
 };
 
+
+Client = this.Client = function(options){
+  this.options = tools.merge({
+    resource: '/',
+    port: 8080,
+    host: 'localhost',
+    log: true,
+    logKey: null,
+    tls: false
+  }, options || {});
+
+  if (this.options.logKey === null) logger.setKey('node.websocket.' + this.options.host + '.' + this.options.port);
+
+  this.handshaked = false;
+  this.data = "";
+  var self = this;
+  var socket = this.socket = tcp.createConnection(this.options.port, this.options.host);
+
+  this.log = this.options.log ? function(message, type){
+    log('[server '+ self.options.host + ':' + self.options.port +'] ' + message, type);
+  } : empty;
+
+  socket.setTimeout(0);
+  socket.setNoDelay(true); // disabling Nagle's algorithm is encouraged for real time transmissions
+  socket.setEncoding('utf8'); // per spec
+  socket.addListener('connect', function(){ self._onConnect(); });
+  socket.addListener('receive', function(data){ self._onReceive(data); });
+  socket.addListener('eof', function(){ self._onDisconnect(); });
+};
+
+Client.prototype._onConnect = function() {
+  this.log('Sending handshake', 'info');
+
+  var self = this;
+  self.socket.send(tools.substitute(requestHeaders.join('\r\n'), {
+    resource: self.options.resource,
+    host: self.options.host,
+    origin: 'file://'
+  }));
+}
+
+Client.prototype._onDisconnect = function() {
+
+}
+
+Client.prototype._onReceive = function(data) {
+  this.log("Received data");
+  if (this.handshaked){
+    this._handle(data);
+  } else {
+    this._handshake(data)
+  }
+}
+
+Client.prototype._handshake = function(data){
+  var self = this,
+      matches = [],
+      module,
+      headers = data.split('\r\n');
+
+  for (var i = 0, l = headers.length, end = responseHeadersMatch.length, match; i < l; i++){
+    if (i === end) break; // handle empty lines that UA send
+    match = headers[i].match(responseHeadersMatch[i]);
+    if (match && match.length > 1){
+      // if there's a capture group, push it into the matches
+      matches.push(match[1]);
+    } else if (!match) {
+      this.log('Handshake aborted. Bad header ' + headers[i]);
+      this.socket.forceClose()
+      return false;
+    }
+  }
+  this.handshaked = true;
+  this.log('Handshake completed', 'info');
+
+  if (this.onConnect) this.onConnect();
+
+  return true;
+};
+
+Client.prototype._handle = function(data){
+  this.data += data;
+
+  chunks = this.data.split('\ufffd');
+  chunk_count = chunks.length - 1; // last chunk is either incomplete or ""
+
+  for (var i = 0; i < chunk_count; i++) {
+    chunk = chunks[i];
+    if (chunk[0] != '\u0000') {
+      this.log('Data incorrectly framed by UA. Dropping connection');
+      this.socket.forceClose();
+      return false;
+    }
+    if (this.onData) this.onData(chunk.slice(1), this);
+  }
+
+  this.data = chunks[chunks.length - 1];
+
+  return true;
+};
+
+Client.prototype.send = function(data) {
+  this.socket.send('\u0000' + data + '\uffff');
+};
+
+
 this.Connection = Connection = function(server, socket){
   this.server = server;
   this.socket = socket;
   this.handshaked = false;
   this.data = "";
-  
+
   this.log = server.options.log ? function(message, type){
     log('[client '+ socket.remoteAddress +'] ' + message, type);
   } : empty;
   this.log('Server created', 'info');
-  
+
   var self = this;
   if (server.options.tls) socket.setSecure();
   socket.setTimeout(0);
@@ -108,21 +232,21 @@ this.Connection = Connection = function(server, socket){
 
 Connection.prototype.onConnect = function(data){
   this.log("Connected", "info")
-    
+
   this.server._onConnect(this);
 };
 
 Connection.prototype._onReceive = function(data){
-  if (this.handshaked){   
-    this._handle(data);    
+  if (this.handshaked){
+    this._handle(data);
   } else {
-    this._handshake(data)    
-  }  
+    this._handshake(data)
+  }
 };
 
 Connection.prototype._onDisconnect = function(){
   this.log("Disconnected", "info")
-    
+
   if (this.module && this.module.onDisconnect) this.module.onDisconnect(this);
   this.socket.close();
   this.server._onDisconnect(this);
@@ -133,7 +257,7 @@ Connection.prototype.send = function(data){
     this.socket.send('\u0000' + data + '\uffff');
   } catch(e) {
     this.socket.close();
-  }  
+  }
 };
 
 Connection.prototype._handle = function(data){
@@ -141,7 +265,7 @@ Connection.prototype._handle = function(data){
 
   chunks = this.data.split('\ufffd');
   chunk_count = chunks.length - 1; // last chunk is either incomplete or ""
-  
+
   for (var i = 0; i < chunk_count; i++) {
     chunk = chunks[i];
     if (chunk[0] != '\u0000') {
@@ -150,7 +274,7 @@ Connection.prototype._handle = function(data){
       return false;
     }
 
-    this.module.onData(chunk.slice(1), this);
+    if (this.module && this.module.onData) this.module.onData(chunk.slice(1), this);
   }
 
   this.data = chunks[chunks.length - 1];
@@ -160,12 +284,12 @@ Connection.prototype._handle = function(data){
 
 Connection.prototype._handshake = function(data){
   this.log('Performing handshake', 'info');
-  
-  var self = this, 
-      matches = [], 
-      module, 
+
+  var self = this,
+      matches = [],
+      module,
       headers = data.split('\r\n');
-  
+
   if (headers.length && headers[0].match(/<policy-file-request.*>/)) {
     // allows Flash based WebSocket implementation to connect.
     // e.g. http://github.com/gimite/web-socket-js
@@ -175,23 +299,23 @@ Connection.prototype._handshake = function(data){
   }
 
   for (var i = 0, l = headers.length, match; i < l; i++){
-    if (i === requestHeaders.length) break; // handle empty lines that UA send 
-    match = headers[i].match(requestHeaders[i]);
+    if (i === requestHeadersMatch.length) break; // handle empty lines that UA send
+    match = headers[i].match(requestHeadersMatch[i]);
     if (match && match.length > 1){
-      // if there's a capture group, push it into the matches          
+      // if there's a capture group, push it into the matches
       matches.push(match[1]);
     } else if (!match) {
-      this.log('Handshake aborted. Bad header ' + headers[i]);          
+      this.log('Handshake aborted. Bad header ' + headers[i]);
       this.socket.close()
       return false;
     }
   }
-  
+
   if (!this.server._verifyOrigin(matches[2])){
     this.log('Handshake aborted. Security policy disallows Origin: ' + matches[2]);
     this.socket.close();
   }
-  
+
   module = './modules' + (matches[0] == '/' ? '/_default' : matches[0]).toLowerCase();
   try {
     var module = require(module);
@@ -201,22 +325,20 @@ Connection.prototype._handshake = function(data){
     this.socket.close();
     return false;
   }
-  
-  if (!this.module.onData){
-    this.log('Module ' + module + '.js doesn\'t implement an onData method.');
-    this.socket.close();
-    return false;
-  }
-  
+
   this.socket.send(tools.substitute(responseHeaders.join('\r\n'), {
     resource: matches[0],
     host: matches[1],
     origin: matches[2],
     protocol: this.server.secure ? 'wss' : 'ws'
   }));
-  
+
   this.handshaked = true;
   this.log('Handshake sent', 'info');
+
+  // call onConnect callback on module
+  if (this.module && this.module.onConnect) this.module.onConnect(this);
+
   return true;
 };
 
